fb8e372361c6bd0a9ce52583aeb9c88019681b1c
Jordan Kiang <jordan.kiang@sagebase.org>
2020-06-17 08:31:32 -0700

    blank line

--- synapseclient/core/utils.py
+++ synapseclient/core/utils.py
@@ -888,3 +888,5 @@ def snake_case(string):
     """Convert the given string from CamelCase to snake_case"""
     # https://stackoverflow.com/a/1176023
     return re.sub(r'(?<!^)(?=[A-Z])', '_', string).lower()
+
+
